a080bf846811a6c9d1043dbc103023268aecd648
hainguyen8086 <hainguyen8086@gmail.com>
2021-04-30 00:47:40 +0700

    update fragment xang + nhot

--- app/src/main/java/com/doancuoiky/myxe/ui/nhot/NhotFragment.java
+++ app/src/main/java/com/doancuoiky/myxe/ui/nhot/NhotFragment.java
@@ -23,13 +23,13 @@ public class NhotFragment extends Fragment {
         nhotViewModel =
                 new ViewModelProvider(this).get(NhotViewModel.class);
         View root = inflater.inflate(R.layout.fragment_nhot, container, false);
-        final TextView textView = root.findViewById(R.id.text_nhot);
-        nhotViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_nhot);
+//        nhotViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
