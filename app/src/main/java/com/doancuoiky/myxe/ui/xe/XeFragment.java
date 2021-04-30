59691763a94b93659b173d2de9c4ded89c5a5ef1
hainguyen8086 <hainguyen8086@gmail.com>
2021-04-30 12:56:12 +0700

    them activiti chi tiet xe

--- app/src/main/java/com/doancuoiky/myxe/ui/xe/XeFragment.java
+++ app/src/main/java/com/doancuoiky/myxe/ui/xe/XeFragment.java
@@ -64,6 +64,7 @@ public class XeFragment extends Fragment {
                 recycleViewXeAdapter = new RecycleViewXeAdapter(getActivity(), tenXe, bienSo);
 
                 recyclerView.setAdapter(recycleViewXeAdapter);
+                
 
             }
         });
