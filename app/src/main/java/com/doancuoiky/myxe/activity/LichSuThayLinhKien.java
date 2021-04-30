5e568fb92b0005e9c9299becbfdaa5bf7918fbd2
hainguyen8086 <hainguyen8086@gmail.com>
2021-04-30 16:29:12 +0700

    add lichsu doxang thay nhot thay linhkien activity

---
+++ app/src/main/java/com/doancuoiky/myxe/activity/LichSuThayLinhKien.java
@@ -0,0 +1,16 @@
+package com.doancuoiky.myxe.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.doancuoiky.myxe.R;
+
+public class LichSuThayLinhKien extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lich_su_thay_linh_kien);
+    }
+}
